42c68fcd6dac4b16af106e2d3579d4d618bf87e0
Lucas Mafra Pinheiro <lucasmafrapinheiro@gmail.com>
2023-05-09 10:58:25 -0300

    Detectando a ordem de instruções e registradores

---
+++ main.py
@@ -0,0 +1,29 @@
+from instruction import Instruction
+from functions import identificar_instrucao
+
+with open('dump', 'r') as f:
+    instrucoes = f.read().splitlines()
+
+binarios = []
+nomes_instrucoes = []
+
+
+for instrucao in instrucoes:
+    binario = bin(int(instrucao, 16))[2:].zfill(32)
+    binarios.append(binario)
+
+instrucoes = []
+for binario in binarios:
+    print(binario)
+    inst = identificar_instrucao(binario)
+    if inst is not None:
+        instrucoes.append(inst)
+    else:
+        print("Instrtucao nao encontrada")
+
+for inst in instrucoes:
+    print(inst.instructionsOrder)
+    print("RD: " + str(inst.rd))
+    print("RS1: " + str(inst.rs1))
+    print("RS2: " + str(inst.rs2))
+
